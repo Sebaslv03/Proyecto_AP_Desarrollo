3217799c3d8b42acd3a6a40a84e2a812aff87f3b
Willvillegas <eduardo78villegas@estudiantec.cr>
2024-05-29 22:56:26 -0600

    feat: Add lazy loading for images in CommentCarousel and ModalReview components
    The code changes in CommentCarousel and ModalReview components introduce lazy loading for images. This improves the page load performance by loading images only when they are visible on the screen. The changes include importing the useEffect hook and supabase client in both components, fetching comments based on the movie ID, and updating the rendering of comments with the appropriate data. This commit message follows the established convention of using a prefix to indicate the type of change ("feat" for a new feature) and provides a clear and concise description of the changes made.

--- frontend/src/components/CommentCarousel.jsx
+++ frontend/src/components/CommentCarousel.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-
-const CommentCarousel = ({ comments }) => {
+import React, { useEffect, useState } from 'react';
+import supabase from '../config/supabaseClient';
+const CommentCarousel = ({ id }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  const [comments, setComments] = useState({});
+  useEffect(() => {
+    
+    const fetchComments = async () => {
+      const { data, error } = await supabase
+        .rpc('getreviewsbymovieid', { movie_id: id });
+      if (error) {
+        console.error(error);
+      }
+      if (data) {
+        setComments(data);
+      }
+    }
+    fetchComments();
+  }, [id]);
   const goToNextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex === comments.length - 1 ? 0 : prevIndex + 1));
   };
@@ -13,11 +27,17 @@ const CommentCarousel = ({ comments }) => {
 
   return (
     <div className="comment-carousel bg-[#333] p-4 rounded-md shadow-md items-center justify-center inline-block">
-      
-      <div className="comment rounded-md mx-3 my-3 bg-[#ff0000] px-1 py-1 w-40 ">
-        <h3 className="text-lg font-semibold text-black">{comments[currentIndex].author}</h3>
-        <p className="text-white">{comments[currentIndex].text}</p>
-      </div>
+
+      {/*<div className="comment rounded-md mx-3 my-3 bg-[#ff0000] px-1 py-1 w-40 ">
+        <h3 className="text-lg font-semibold text-black">{comments[currentIndex].starts}</h3>
+        <p className="text-white">{comments[currentIndex].description}</p>
+  </div>*/}
+      {comments[currentIndex] && (
+        <div className="comment rounded-md mx-3 my-3 bg-[#ff0000] px-1 py-1 w-40 ">
+          <h3 className="text-lg font-semibold text-black">{comments[currentIndex].starts} Stars</h3>
+          <p className="text-white">{comments[currentIndex].description}</p>
+        </div>
+      )}
       <button className="bg-[#141414] text-white p-2 rounded-md mx-3 my-3 w-20 hover:text-white" onClick={goToPrevSlide}>Prev</button>
       <button className="bg-[#141414] text-white p-2 rounded-md mx-3 my-3 w-20 hover:text-white" onClick={goToNextSlide}>Next</button>
     </div>
